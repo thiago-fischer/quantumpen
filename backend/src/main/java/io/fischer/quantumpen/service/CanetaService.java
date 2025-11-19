9364363f6c470a3fb41253812499f61f9007d9ec
Thiago Fischer <thiagopbf16@gmail.com>
2025-11-19 00:22:05 -0300

    Adicionando Logger em CanetaService

--- backend/src/main/java/io/fischer/quantumpen/service/CanetaService.java
+++ backend/src/main/java/io/fischer/quantumpen/service/CanetaService.java
@@ -10,10 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class CanetaService {
 
+    private final Logger logger = Logger.getLogger(CanetaService.class.getName());
     private final CanetaRepository repository;
 
     public CanetaService(CanetaRepository repository) {
@@ -21,6 +23,7 @@ public class CanetaService {
     }
 
     public List<CanetaResponseDTO> encontrarTodos() {
+        logger.info("Listando todas as canetas!");
         return CanetaResponseDTO.fromEntities(repository.findAll());
     }
 
@@ -28,11 +31,13 @@ public class CanetaService {
         Caneta entity = repository.findById(id).orElseThrow(
                 () -> new NotFoundException("Caneta com o ID " + id + " não encontrada!")
         );
+        logger.info("Buscando uma caneta!");
         return CanetaResponseDTO.fromEntity(entity);
     }
 
     public CanetaResponseDTO criarCaneta(CreateCanetaDTO canetaDTO) {
         Caneta entity = repository.save(canetaDTO.toEntity());
+        logger.info("Criando uma caneta!");
         return CanetaResponseDTO.fromEntity(entity);
     }
 
@@ -42,6 +47,7 @@ public class CanetaService {
         );
         canetaDTO.applyTo(entity);
         Caneta updatedEntity = repository.save(entity);
+        logger.info("Editando uma caneta!");
         return CanetaResponseDTO.fromEntity(updatedEntity);
     }
 
@@ -50,6 +56,7 @@ public class CanetaService {
                 () -> new NotFoundException("Caneta com o ID " + id + " não encontrada!")
         );
         repository.delete(entity);
+        logger.info("Deletando uma caneta!");
         return ResponseEntity.noContent().build();
     }
 }
